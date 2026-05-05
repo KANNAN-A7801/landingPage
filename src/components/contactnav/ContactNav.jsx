a03469b17e3c8e00e70cef274897aca4d106e398
foujaanjs <javithfoujaan@gmail.com>
2026-05-05 17:24:23 +0530

    new-deploy-change

--- src/components/contactnav/ContactNav.jsx
+++ src/components/contactnav/ContactNav.jsx
@@ -25,7 +25,7 @@ const ContactNav = () => {
     setIsSubmitting(true);
 
     try {
-      const response = await fetch('https://landingpage-r9w7.onrender.com/api/contact/send', {
+      const response = await fetch('https://landingpage-1-d46p.onrender.com/api/contact/send', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
